9fb4a5d8a6ac40e873b73d3360b2e1ed749a13ce
Uli Kusterer <ulivc@zathras.de>
2018-03-12 20:33:50 +0100

    Initial Commit

---
+++ ULITwitchbot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ULITwitchbot
+//
+//  Created by Uli Kusterer on 12.03.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ULITwitchbot/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ULITwitchbot
+//
+//  Created by Uli Kusterer on 12.03.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
